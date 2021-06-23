b356ee840b502808efcdfdbd96bca47ca6005699
yeabu369 <yeabsdebe@gmail.com>
2021-06-23 12:30:57 +0000

    Added Association with Seat and Hall Models

--- models/row.js
+++ models/row.js
@@ -1,24 +1,36 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
+
   class Row extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Seat, Hall }) {
       // define association here
+      this.hasMany(Seat);
+      this.belongsTo(Hall);
     }
   };
+
   Row.init({
-    Number: DataTypes.INTEGER,
-    Seats: DataTypes.INTEGER
+    Number: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    TotalSeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Row',
   });
+
   return Row;
+
 };
