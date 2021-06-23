43b995187c0e21d1acc1e08e3a7ecb42f17e2fcb
yeabu369 <yeabsdebe@gmail.com>
2021-06-23 11:17:09 +0000

    Added Movie Associations to Genre Model

--- models/genre.js
+++ models/genre.js
@@ -1,16 +1,20 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
+
   class Genre extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Movie }) {
       // define association here
+      this.belongsToMany(Movie, {
+        through: 'MovieGenre'
+      });
     }
   };
   Genre.init({
