b5b01dc85aa5ecaa7a55ffc60b26c94b9c5072ae
yeabu369 <yeabsdebe@gmail.com>
2021-06-23 11:16:43 +0000

    Added genre association to Movie Model

--- models/movie.js
+++ models/movie.js
@@ -10,19 +10,33 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Genre }) {
       // define association here
+      this.belongsToMany(Genre, {
+        through: 'MovieGenre',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
     }
   };
 
   Movie.init({
-    Title: DataTypes.STRING,
-    DurationInHours: DataTypes.INTEGER,
-    Type: DataTypes.STRING,
-    DebutsAt: DataTypes.DATE,
-    ExpiresAt: DataTypes.DATE,
-    Synopsis: DataTypes.TEXT,
-    Likes: DataTypes.INTEGER
+    Title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    DurationInMin: {
+      type: DataTypes.INTEGER
+    },
+    DebutsAt: {
+      type: DataTypes.DATE
+    },
+    ExpiresAt: {
+      type: DataTypes.DATE
+    },
+    Synopsis: {
+      type: DataTypes.TEXT
+    },
   }, {
     sequelize,
     modelName: 'Movie',
