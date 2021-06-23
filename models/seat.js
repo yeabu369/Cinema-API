07eaea28d40d9a8b0c65b9cc3663d2c184c57ea1
yeabu369 <yeabsdebe@gmail.com>
2021-06-23 12:31:13 +0000

    Added association with Role Model

--- models/seat.js
+++ models/seat.js
@@ -10,8 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Row }) {
       // define association here
+      this.belongsTo(Row);
     }
   };
 
