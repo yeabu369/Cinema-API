6ee3b2b888edca2bbc23e2913c946934d759546c
yeabu369 <yeabsdebe@gmail.com>
2021-06-24 14:44:17 +0300

    Added hasOne association

--- models/seat.js
+++ models/seat.js
@@ -10,8 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Ticket }) {
       // define association here
+      this.hasOne(Ticket, {
+        foreignKey: 'SeatId'
+      });
     }
   };
 
